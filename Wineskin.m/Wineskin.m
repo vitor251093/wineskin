845ee19f3a609a893d3e16108a86db042d3290ef
doh123 <doh123@Optimus.local>
2011-03-17 18:40:30 -0500

    Stupid typos

--- Wineskin.m/Wineskin.m
+++ Wineskin.m/Wineskin.m
@@ -1,5 +1,5 @@
 //  Wineskin.m
-//  Copyright 2010 by The Wineskin Project and doh123@doh1223.com. All rights reserved.
+//  Copyright 2011 by The Wineskin Project and doh123@doh123.com All rights reserved.
 //  Licensed for use under the LGPL <http://www.gnu.org/licenses/lgpl-2.1.txt>
 
 #import <Cocoa/Cocoa.h>
--- WineskinLauncher/WineskinLauncherAppDelegate.m
+++ WineskinLauncher/WineskinLauncherAppDelegate.m
@@ -2,7 +2,7 @@
 //  WineskinLauncherAppDelegate.m
 //  WineskinLauncher
 //
-//  Copyright 2010 by The Wineskin Project and doh123@doh1223.com. All rights reserved.
+//  Copyright 2011 by The Wineskin Project and doh123@doh123.com All rights reserved.
 //  Licensed for use under the LGPL <http://www.gnu.org/licenses/lgpl-2.1.txt>
 //
 
