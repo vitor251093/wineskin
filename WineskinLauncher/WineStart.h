f9473fcdccfc213ca6a3606c484509852e50300a
doh123 <doh123@doh123.com>
2014-03-20 17:15:00 -0500

    make Mac Driver capture display by default

---
+++ WineskinLauncher/WineStart.h
@@ -0,0 +1,13 @@
+//
+//  WineStart.h
+//  WineskinLauncher
+//
+//  Created by doh123 on 3/20/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WineStart : NSObject
+
+@end
---
+++ WineskinLauncher/WineStart.m
@@ -0,0 +1,13 @@
+//
+//  WineStart.m
+//  WineskinLauncher
+//
+//  Created by doh123 on 3/20/14.
+//
+//
+
+#import "WineStart.h"
+
+@implementation WineStart
+
+@end
